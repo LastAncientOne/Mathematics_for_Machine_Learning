d3f9febb91fe0bff0373262d34aa76b97e9709fd
LastAncientOne <lastancientone@gmail.com>
2019-05-31 08:43:20 -0700

    Update Volume_SurfaceArea_Apps.py

--- Math Apps Software/Volume_SurfaceArea_Apps.py
+++ Math Apps Software/Volume_SurfaceArea_Apps.py
@@ -1,1 +1,35 @@
 
+import math
+
+options = " Rectangular Prism, General Prisms, Right Circular Cylinder, Square Pyramid, Right Circular Cone, Sphere,Quit".split(",")
+
+def Rectangular Prism():
+    length = float(input(print("Enter the length:"))
+    width = float(input(print("Enter the width:"))
+    height = float(input(print("Enter the height:"))
+    Volume = length * width * height
+    SA = 2*(length*width) + 2*(height*width) + 2*(length*height)
+    print('Volume of a Rectangular: ', Volume)
+    print('Surface Area of a Rectangular: ', SA)
+    return
+                   
+def main():
+    run_program = True
+    while run_program:
+        print("Volume and Area Surface")
+        print("\n")
+        print("Choose Option:")
+        for i in range(1, len(options)+1):
+            print("{} - {}".format(i, options[i-1]))
+        choice = int(input())
+        
+        if choice == 1:            
+            print('_____Rectangular Prism_____') 
+            Rectangular Prism()
+        
+        elif choice == 2:
+             run_program = False             
+
+
+if __name__ == "__main__":
+    main()
